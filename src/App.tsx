a2a34f44b3ecf5e577e4bd849f8fd55280c5e77e
Abdul-Qoyyum <141515341+QoyyumO@users.noreply.github.com>
2024-07-11 12:27:54 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ const App: React.FC = () => {
       <Header />
       <main>
         <Routes>
+           <Route path="/" element={<Portfolio />} />
           <Route path="/home" element={<Portfolio />} />
           <Route path="/projects" element={<Slider />} />
         </Routes>
